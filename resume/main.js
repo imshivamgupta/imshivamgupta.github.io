7a43601dc3ff7b37aa5c8ee1e2a966105580c237
shivam gupta <shivamg8797@gmail.com>
2019-01-09 12:57:43 +0530

    resume.pdf upload

--- resume/main.js
+++ resume/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let resume = document.querySelector('.btn-resume');
     let hireMe = document.querySelector('.btn-hire');
     resume.addEventListener('click', () => {
-        window.location = './';
+        window.location = './resume.pdf';
     });
     hireMe.addEventListener('click', () => {
         window.location = 'mailto:shivamg8797@gmail.com';
